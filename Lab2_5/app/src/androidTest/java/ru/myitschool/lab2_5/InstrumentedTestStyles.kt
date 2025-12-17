883995685ab07daa369c0cc54c2554181c91e58d
nikitanikonov <loser.alanwolker2005@gmail.com>
2025-12-17 23:18:13 +0300

    Initial commit

---
+++ Lab2_5/app/src/androidTest/java/ru/myitschool/lab2_5/InstrumentedTestStyles.kt
@@ -0,0 +1,13 @@
+package ru.myitschool.lab2_5
+
+import androidx.test.ext.junit.runners.AndroidJUnit4  // Правильный импорт
+import androidx.test.filters.MediumTest
+import org.junit.*
+import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+
+
+@RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@MediumTest
+class InstrumentedTestStyles {}
